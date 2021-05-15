dc74c7db24ff90b6c5ad4537801ca21a17315772
RitaDyvnych <m.dyvnych@gmail.com>
2021-05-15 15:11:46 +0300

    исправлен скрипт для работы кнопок read-more

--- src/read-more.js
+++ src/read-more.js
@@ -2,13 +2,19 @@
         const refs = {
             openModalBtn: document.querySelector('[data-button-open]'),
             closeModalBtn: document.querySelector('[data-button-close]'),
+            openWrapBtn: document.querySelector('[data-wrap-open]'),
+            closeWrapBtn: document.querySelector('[data-wrap-close]'),
             modal: document.querySelector('[data-wrap]'),
+            wrap: document.querySelector('[data-wrap1]'),
         };
 
         refs.openModalBtn.addEventListener('click', toggleModal);
         refs.closeModalBtn.addEventListener('click', toggleModal);
+        refs.openWrapBtn.addEventListener('click', toggleModal);
+        refs.closeWrapBtn.addEventListener('click', toggleModal);
 
         function toggleModal() {
             refs.modal.classList.toggle('is-hidden');
+            refs.wrap.classList.toggle('is-hidden');
         }
     })();
