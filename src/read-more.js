91c53acbbd2ff7bf2d796f4d601c890899e009dd
RitaDyvnych <m.dyvnych@gmail.com>
2021-05-19 19:29:06 +0300

    отредактировала высоту появляющихся блоков read more на двух секциях

--- src/read-more.js
+++ src/read-more.js
@@ -3,7 +3,6 @@
             openModalBtn: document.querySelector('[data-button-open]'),
             closeModalBtn: document.querySelector('[data-button-close]'),
             modal: document.querySelector('[data-wrap]'),
-            animate1: document.querySelector('[data-anime1]'),
         };
 
         refs.openModalBtn.addEventListener('click', toggleModal);
@@ -11,7 +10,6 @@
 
         function toggleModal() {
             refs.modal.classList.toggle('is-hidden');
-            refs.animate1.classList.toggle('animate__slideInDown');
         }
 })();
     
--- src/read-more2.js
+++ src/read-more2.js
@@ -3,7 +3,6 @@
             openWrapBtn: document.querySelector('[data-wrap-open]'),
             closeWrapBtn: document.querySelector('[data-wrap-close]'),
             wrap: document.querySelector('[data-wrap1]'),
-            animate: document.querySelector('[data-anime]'),
         };
 
         refs.openWrapBtn.addEventListener('click', toggleModal);
@@ -11,7 +10,6 @@
 
         function toggleModal() {
             refs.wrap.classList.toggle('is-hidden');
-            refs.animate.classList.toggle('animate__slideInDown');
         }
 })();
     
